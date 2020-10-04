99f85c9925ec5bd799c94cc2f055db4216709691
Dhairya <dhairyasharma7120@gmail.com>
2020-10-04 10:33:40 +0530

    created ecommerce website with react app and this is first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            <div className="item">
+                <div className="item-content">
+                    <h1>HATS</h1>
+                    <span>SHOP NOW</span>
+                </div>    
+            </div>
+            <div className="item">
+                <div className="item-content">
+                    <h1>JACKETS</h1>
+                    <span>SHOP NOW</span>
+                </div>    
+            </div>
+            <div className="item">
+                <div className="item-content">
+                    <h1>SNEAKERS</h1>
+                    <span>SHOP NOW</span>
+                </div>    
+            </div>
+            <div className="item">
+                <div className="item-content">
+                    <h1>WOMENS</h1>
+                    <span>SHOP NOW</span>
+                </div>    
+            </div>
+            <div className="item">
+                <div className="item-content">
+                    <h1>MENS</h1>
+                    <span>SHOP NOW</span>
+                </div>    
+            </div>            
+        </div>
+    )
+}
+
+export default HomePage
