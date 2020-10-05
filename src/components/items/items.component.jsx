cfde468f01eb5128207e8d2316a0fa9617574c91
Dhairya <dhairyasharma7120@gmail.com>
2020-10-05 13:25:20 +0530

    (Day 2 : 01:24 PM) css modifications

--- src/components/items/items.component.jsx
+++ src/components/items/items.component.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './items.styles.css'
+
 const Items = ({ title, imageUrl, size })=>(
 
-     <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }}  className={`${size} items`}>
+     <div className={`${size} items`}>
+        
+        <div style={{ backgroundImage: `url(${imageUrl})`}} className="bg-img"></div>
+
         <div className="item-content">
-            <h1>{title}</h1>
+            <h1>{title.toUpperCase()}</h1>
             <span>SHOP NOW</span>
-        </div>    
+        </div>
+
     </div>
 );
 
