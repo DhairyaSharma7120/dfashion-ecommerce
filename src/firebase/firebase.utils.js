f4ef91f7f14fae3a5b3305e1e749432784f1c364
Dhairya <dhairyasharma7120@gmail.com>
2020-10-08 08:35:40 +0530

    (Day 5 : 8:15 AM) config firebase and added signin with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAM6PPa-0Py-YjPMOW4S9PN0N9ep5TumF4",
-    authDomain: "dfashion-db.firebaseapp.com",
-    databaseURL: "https://dfashion-db.firebaseio.com",
-    projectId: "dfashion-db",
-    storageBucket: "dfashion-db.appspot.com",
-    messagingSenderId: "861829670545",
-    appId: "1:861829670545:web:82ed0bd9f2757b374b0130",
-    measurementId: "G-D5ZLDCSZV9"
+    //  Your firebase config here
   };
 
 
