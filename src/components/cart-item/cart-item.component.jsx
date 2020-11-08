d2e9cbdb6d27ab577a33dc761ccaf46dd51034eb
Dhairya <dhairyasharma7120@gmail.com>
2020-11-08 21:29:14 +0530

    directory reducer added

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,9 +5,9 @@ import './cart-item.styles.scss';
 const CartItem  = ({item: { imageUrl, price, name, quantity   }}) => (
     <div className="cart-item">
         <img src={imageUrl} alt="productpic"/>
-        <div classname="item-details">
-            <span classname="name">{name}</span><br/>
-<span classname="price">Price: {quantity} x {price}</span>
+        <div className="item-details">
+            <span className="name">{name}</span><br/>
+<span className="price">Price: {quantity} x {price}</span>
         </div>
     </div>
 )
